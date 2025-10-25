abb57ddeb9abbc9e8efd98f21b770e32d9e924d9
Log <quzhizhanghuzp@163.com>
2025-10-25 10:08:57 +0800

    Delete push_to_github.bat

--- push_to_github.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Attempting to push to GitHub...
-git push origin main
-if %errorlevel% neq 0 (
-    echo Push failed, trying alternative method...
-    git push --set-upstream origin main
-)
-pause
